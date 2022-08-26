a250a1f118f40a91f75d7ed5f35dbf18aec55d01
Harley Johnson <hjohnson@uwm.com>
2022-08-26 14:23:56 -0400

    Added fonts. Started setting up color for theme.

---
+++ src/common/constants/theme-values.js
@@ -0,0 +1,53 @@
+
+
+export const Palette = {
+  mode: 'light',
+  common: {
+    black: '#000',
+    white: '#fff',
+  },
+  primary: {
+    main: 'hsl(271, 100%, 50%)',
+    light: 'hsl(271, 100%, 67%)',
+    dark: 'hsl(271, 100%, 30%)',
+    contrastText: '#fff',
+  },
+  secondary: {
+    main: 'hsl(271, 100%, 50%)',
+    light: 'hsl(271, 100%, 67%)',
+    dark: 'hsl(271, 100%, 30%)',
+    contrastText: '#fff',
+  },
+  accent: {
+    main: 'hsl(271, 100%, 50%)',
+    light: 'hsl(271, 100%, 67%)',
+    dark: 'hsl(271, 100%, 30%)',
+    contrastText: '#fff',
+  },
+
+  error: {
+    main: 'hsl(305, 100%, 50%)',
+    light: 'hsl(305, 100%, 78%)',
+    dark: 'hsl(305, 100%, 32%)',
+    contrastText: '#fff',
+  },
+  warning: {
+    main: 'hsl(305, 100%, 50%)',
+    light: 'hsl(305, 100%, 78%)',
+    dark: 'hsl(305, 100%, 32%)',
+    contrastText: '#fff',
+  },
+  info: {
+    main: 'hsl(271, 100%, 50%)',
+    light: 'hsl(271, 100%, 67%)',
+    dark: 'hsl(271, 100%, 30%)',
+    contrastText: '#fff',
+  },
+  success: {
+    main: 'hsl(153, 100%, 50%)',
+    light: 'hsl(153, 100%, 75%)',
+    dark: 'hsl(153, 100%, 35%)',
+    contrastText: 'hsl(271, 100%, 30%)',
+  },
+
+}
