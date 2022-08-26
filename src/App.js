e183f3a1658bac828acf50573ea4ec7681314e5e
Harley Johnson <hjohnson@uwm.com>
2022-08-26 15:57:19 -0400

    Deleted unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './theme/styles/css/theme.css';
 import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
